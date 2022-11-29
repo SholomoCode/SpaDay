1c4b70d32a35253f9db2b66f7427b9d053d00220
SholomoCode <worksolom5@gmail.com>
2022-11-29 12:00:43 -0600

    done

---
+++ SpaDay/ViewModel/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModel
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match! Try again")]
+        public string VerifyPassword { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
